d5b745555004d2665231e42ea987db24f6b37e64
Nayak Ramulu <121868827+NayakRamulu50@users.noreply.github.com>
2024-01-09 20:25:53 +0530

    Add files via upload
    adding initial files

---
+++ script.js
@@ -0,0 +1,73 @@
+const accesskey = "sO4Vt1-6FOYQgXfoD7ZNnrhDc8JmmVKuwII2xrz4tpM";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+
+
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+    
+    const results = data.results;
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    showMoreBtn.style.display = "block";
+
+}
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click", ()=>{
+    page++;
+    searchImages();
+})
+
+const images = [
+    '1.png',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.png',
+    '7.png',
+    '8.png',
+    '9.png',
+    '10.jpg',
+    '11.jpg',
+    '12.jpg',
+  ];
+
+  let currentIndex = 0;
+
+  function changeBackground() {
+    document.body.style.backgroundImage = `url(${images[currentIndex]})`;
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+
+
+  
